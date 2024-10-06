cde1d9e7ad86a6d935c850d684d631777504a43e
Justin Smith <justinsmith@MacBook-Pro-3.lan>
2024-10-06 11:34:46 -0400

    Working

--- forms.py
+++ forms.py
@@ -8,6 +8,15 @@ class MessageForm(FlaskForm):
 
     text = TextAreaField('text', validators=[DataRequired()])
 
+class UserEditForm(FlaskForm):
+    """Form for editing users."""
+
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[Length(min=6)])
+    image_url = StringField('(Optional) Image URL')
+    header_image_url = StringField('(Optional) Image URL')
+    bio = StringField('Some bio')
 
 class UserAddForm(FlaskForm):
     """Form for adding users."""
